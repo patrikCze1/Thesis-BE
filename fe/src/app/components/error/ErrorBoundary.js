ac0938b279f5e17020db394b18a24f2ecd817ada
patrikCze1 <padr.stipek@gmail.com>
2022-03-30 16:21:43 +0200

    update err boundary

--- fe/src/app/components/error/ErrorBoundary.js
+++ fe/src/app/components/error/ErrorBoundary.js
@@ -4,20 +4,22 @@ import ErrorBoundaryContent from "./ErrorBoundaryContent";
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: null };
   }
 
   static getDerivedStateFromError(error) {
-    return { hasError: true };
+    return { hasError: true, error };
   }
 
   componentDidCatch(error, errorInfo) {
+    this.setState({ error, hasError: true });
+
     console.error("err boundary", error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      return <ErrorBoundaryContent />;
+      return <ErrorBoundaryContent error={this.state.error} />;
     }
 
     return this.props.children;
--- fe/src/app/components/error/ErrorBoundaryContent.js
+++ fe/src/app/components/error/ErrorBoundaryContent.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { NavLink } from "react-router-dom";
 
-export default function ErrorBoundaryContent() {
+export default function ErrorBoundaryContent({ error }) {
   const { t } = useTranslation();
 
   return (
-    <div>
+    <div
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
       <h1>{t("error.errorAppeared")}</h1>
       <a href="/">{t("message.goToMainPage")}</a>
+
+      <p className="text-danger mt-2 text-center">
+        Detail chyby: <br /> {error?.message}
+      </p>
     </div>
   );
 }
